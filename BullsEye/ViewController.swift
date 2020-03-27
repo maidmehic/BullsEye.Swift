c68cc188ca4403e6116e7b57fe48cc71111838c7
Maid Mehic <maid.mehic@compusight.com>
2020-03-27 05:03:10 -0700

    Wired up controls with variables

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,25 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var targetValue: Int = 0
+    var scoreValue: Int = 0
+    var roundValue: Int = 0
+    
+    @IBOutlet weak var targetValueView: UILabel!
+    @IBOutlet weak var scoreValueView: UILabel!
+    @IBOutlet weak var roundValueView: UILabel!
+    @IBOutlet weak var sliderView: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound(sliderVal: 0)
+    }
+    
+    func startNewRound(sliderVal:Int) {
+        calculateScore(sliderVal: sliderVal)
+        roundValue+=1
+        targetValue=Int.random(in: 1...100)
+        targetValueView.text=String(targetValue)
+        sliderView.value=50
+        scoreValueView.text=String(scoreValue)
+        roundValueView.text=String(roundValue)
+    }
+    
+    func calculateScore(sliderVal:Int){
+        if(sliderVal==0){//if new game, set score to 0
+           scoreValue=sliderVal
+        }
+        else{
+          scoreValue+=sliderVal
+        }
+    }
+    
+    @IBAction func startNewGame() {
+        roundValue=0
+        targetValue=0
+        scoreValue=0
+        startNewRound(sliderVal: 0)
     }
 
     @IBAction func showAlert(){
+        let sliderValue = Int(sliderView.value.rounded())//get value of slider when click Hit Me! button
+       
         let alert = UIAlertController(
-            title: "Hello world",
-            message: "This is AlertController",
-            preferredStyle: .actionSheet)
+            title: "Score",
+            message:"Slider value is \(sliderValue) \n Target value is \(targetValue)",
+            preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Great", style: .default, handler: nil)
+        let action = UIAlertAction(title: "Okay", style: .default, handler: {
+            (UIAlertAction) in self.startNewRound(sliderVal: sliderValue)
+        })
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-    }
-    
-    func onActionRespond(){
-        print("CLICKED")
     }
 }
 
